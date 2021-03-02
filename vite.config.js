07bde99e6a06406c7cdfa0224b35766cf957a952
leiyiyi <leiyibeyond@qq.com>
2021-03-02 16:44:53 +0800

    add build.target option

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,12 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   plugins: [
     vue()
-  ]
+  ],
+  // Server Options
+  // server: {},
+  // Build Options
+  build: {
+    // 'modules' or 'esnext' or array->(valid: esN, chromeN, edgeN, firefoxN, iosN, nodeN, safariN)
+    target: ['es2020', 'chrome88']
+  }
 })
