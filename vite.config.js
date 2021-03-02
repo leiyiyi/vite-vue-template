7908cfd083585cd239b22897e779351abd167a74
leiyiyi <leiyibeyond@qq.com>
2021-03-02 16:48:06 +0800

    add resolve.alias option

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,4 @@
+import { resolve } from 'path'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
@@ -6,6 +7,12 @@ export default defineConfig({
   plugins: [
     vue()
   ],
+  // Resolve Options
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, './src')
+    }
+  },
   // Server Options
   // server: {},
   // Build Options
